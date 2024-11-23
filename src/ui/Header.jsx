1392d8f31add70917f6902b9fd587f29a6da26d7
Shukra Tamang <shukratamang.npl@gmail.com>
2024-11-23 15:24:23 +1100

    fetch order list and complete order page

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,9 +1,11 @@
 import { Link } from "react-router-dom";
+import SearchOrder from "../features/order/SearchOrder";
 
 const Header = () => {
   return (
     <header>
       <Link to="/">PizzaHub</Link>
+      <SearchOrder />
     </header>
   );
 };
